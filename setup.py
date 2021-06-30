1c83f8013dd504594515adf49ef071241f91b1b9
Drew Bollinger <drew@developmentseed.org>
2021-06-30 14:08:17 -0400

    add dependencies, close #1

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ description='Testing installation of Package',
 url='#',
 author=['Ajay','Tarun'],
 author_email=['krishajay.g@email.com','tarunn2799@gmail.com'],
-install_requires=['torch','matplotlib','numpy','tqdm','torchvision','sklearn','sklearn','pandas','scipy', 'Pillow', 'tensorboardX'],
+install_requires=['torch','matplotlib','numpy','tqdm','torchvision','sklearn','sklearn','pandas','scipy', 'Pillow', 'tensorboardX', 'opencv-python', 'umap'],
 packages=['InteractivePlot'],
 zip_safe=False)
